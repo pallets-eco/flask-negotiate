45736b26753053fb43e463e75c1eea4ae55ac134
Matt Wright <mdw1980@gmail.com>
2012-07-19 12:05:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 """
 Flask-Negotiate
---------------
+===============
 
-Content negotiation utility for Flask apps
+Content negotiation utility for Flask apps.
 
-Links
-`````
 
-* `development version
-  <https://github.com/mattupstate/flask-negotiate/raw/develop#egg=Flask-Negotiate-dev>`_
+Resources
+---------
+
+- `Documentation <http://packages.python.org/Flask-Negotiate/>`_
+- `Issue Tracker <http://github.com/mattupstate/flask-negotiate/issues>`_
+- `Code <http://github.com/mattupstate/flask-negotiate/>`_
+- `Development Version
+  <http://github.com/mattupstate/flask-negotiate/zipball/develop#egg=Flask-Negotiate-dev>`_
 
 """
 from setuptools import setup
